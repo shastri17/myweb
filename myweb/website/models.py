162669c0b2f122890b2e2b76a21a90287d67bb0e
shastri17 <ganesh.s.shastri@gmail.com>
2017-02-23 21:41:02 +0530

    made blog model

--- myweb/website/models.py
+++ myweb/website/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+	head1=models.CharField(max_length=120,blank=False)
+	para1=models.TextField(blank=True)
+	para2=models.TextField(blank=True)
+	para3=models.TextField(blank=True)
+	def __str__(self):
+ 		return self.head1
