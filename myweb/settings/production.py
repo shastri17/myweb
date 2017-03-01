f7f8bc164c4d8ff8940b9eecbfb9ae7bde578361
shastri17 <ganesh.s.shastri@gmail.com>
2017-03-01 21:07:53 +0530

    hi

--- myweb/settings/production.py
+++ myweb/settings/production.py
@@ -1,7 +1,7 @@
 import os
 from myweb.settings.base import *
 
-ALLOWED_HOSTS=['52.43.69.172','localhost']
+ALLOWED_HOSTS=['172.31.31.158','localhost']
 
 STATIC_ROOT=os.environ.get('STATIC_ROOT',None)
 
