8a03e78e8886e0f93d19a5102a1b4610e0356e8e
ganesh <shastri@ip-172-31-31-158.us-west-2.compute.internal>
2017-03-02 16:34:12 +0000

    server up

---
+++ myweb/settings/home.py
@@ -0,0 +1,4 @@
+import os
+from myweb.settings.base import *
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
--- myweb/settings/production.py
+++ myweb/settings/production.py
@@ -1,7 +1,6 @@
 import os
 from myweb.settings.base import *
-
-ALLOWED_HOSTS=['52.43.69.172','localhost']
-
-STATIC_ROOT=os.environ.get('STATIC_ROOT',None)
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ALLOWED_HOSTS = ['52.43.69.172','localhost']
+STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
 
