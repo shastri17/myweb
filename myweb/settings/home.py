9eee45a3896195bc52a7726e10fc5a585aa8d51c
shastri17 <ganesh.s.shastri@gmail.com>
2017-04-17 21:04:00 +0530

    changed project

--- myweb/settings/home.py
+++ myweb/settings/home.py
@@ -1,4 +1,27 @@
 import os
 from myweb.settings.base import *
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = '/home/shastri/projects/myweb'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
