3d2c3f9fff0fd6f9062d0bc7941fe4a43eb09e27
ganesh <shastri@ip-172-31-31-158.us-west-2.compute.internal>
2017-04-17 14:33:09 +0000

    nothing

--- myweb/settings/production.py
+++ myweb/settings/production.py
@@ -1,6 +1,6 @@
 import os
 from myweb.settings.base import *
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ALLOWED_HOSTS = ['52.43.69.172','localhost']
+ALLOWED_HOSTS = ['52.43.69.172','localhost','ganeshshastri.me']
 STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
 
